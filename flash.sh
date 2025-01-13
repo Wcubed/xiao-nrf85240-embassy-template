38677ce4e209e3ae409040bedeabaf57418678c9
Wybe Westra <wybe@ruurdwestra.nl>
2025-01-13 20:58:05 +0100

    Working example over USB.

---
+++ flash.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo
+
+# To figure out what the device name is:
+# plug the device in. Double press the reset button.
+# Run `dmesg` and it should list something like:
+# [sda] Attached SCSI removable disk
+COM_PORT=/dev/ttyACM0
+CRATE=$CARGO_PKG_NAME
+
+arm-none-eabi-objcopy -O ihex target/thumbv7em-none-eabi/release/$CRATE target/$CRATE.hex
+adafruit-nrfutil dfu genpkg --dev-type 0x0052 --sd-req 0x0123 --application target/$CRATE.hex target/$CRATE.zip
+adafruit-nrfutil --verbose dfu serial -pkg target/$CRATE.zip -p $COM_PORT -b 115200 --singlebank
